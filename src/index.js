cfed181f9c98565e262a000b43a6ee46f577663a
Marlom <falconsoft.3d@gmail.com>
2021-03-02 16:35:51 +0100

    sc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+const axios = require('axios');
+
 const greet = name => 'Hello' + name;
 
-const user = () => {
-     
+const users = async () => {
+     const res =  await axios.get('https://jsonplaceholder.typicode.com/users');
+     return res.data;
+}
+
+// users().then( res=>console.log(res) );
+
+module.export = {
+    greet,
+    users
 }
